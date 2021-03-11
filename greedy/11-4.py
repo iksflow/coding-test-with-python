0da15082825138a8a393ccfb8a832b06ae006214
iksflow <theiksflow@gmail.com>
2021-03-11 23:17:40 +0900

    그리디 알고리즘 문제풀이

--- greedy/11-4.py
+++ greedy/11-4.py
@@ -2,6 +2,16 @@
 
 n = int(input())
 array = list(map(int, input().split()))
+array.sort()
 
-print('array =', array)
+target = 1
+for x in array:
+    # 만들 수 없는 금액을 찾았을 때 반복 종료
+    if target < x:
+        break
+    target += x
+
+# 만들 수 없는 금액 출력
+print(target)
+
 
